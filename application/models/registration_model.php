63e62564f6cce56859dcc7313d19053deb280b3f
aldirahmanws <rahmanaldi290@gmail.com>
2018-07-05 15:22:02 +0700

    asd
    dsa

--- application/models/registration_model.php
+++ application/models/registration_model.php
@@ -23,7 +23,7 @@ class Registration_model extends CI_Model {
            $kode = 1;    
           }
           $kodemax = str_pad($kode, 3, "0", STR_PAD_LEFT); // angka 4 menunjukkan jumlah digit angka 0
-          $kodejadi = "KO".$kodemax;    // hasilnya ODJ-9921-0001 dst.
+          $kodejadi = "PE".$kodemax;    // hasilnya ODJ-9921-0001 dst.
           return $kodejadi; 
     }
 
