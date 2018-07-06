1489ded62f5ab3982b64a957786e164219ca3a5c
aldirahmanws <rahmanaldi290@gmail.com>
2018-07-06 13:21:54 +0700

    asd
    sad

--- application/views/tes.php
+++ application/views/tes.php
@@ -1,9 +1,15 @@
-<div class="py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12">
-        	<p><img src="http://jic.ac.id/v2/images/jic-logo.png" alt="" width="124" height="50" /><p class="pull-right">Dateasdasasddddddddddddd</p>&nbsp;Jl.Perunggu No 53-54<br />&nbsp;Cempak Putih<br />&nbsp;Jakarta Pusat 10640</p>
-            
-        </div>
-    </div>
+<div class="col-md-12">
+	<div class="box">
+
+	<div class="box-body">
+      
+
+        
+        	<p>
+            <img src="http://jic.ac.id/v2/images/jic-logo.png" alt="" width="124" height="50" />
+            <span class="pull-right">Date: 2/10/2014</span>
+        
+          <br>Jl.Perunggu No 53-54<br />&nbsp;Cempak Putih<br />&nbsp;Jakarta Pusat 10640</p>
   </div>
+</div>
+</div>
