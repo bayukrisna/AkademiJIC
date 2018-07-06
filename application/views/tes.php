eb42e1cd354e939729ffb730800ea0e009f59274
aldirahmanws <rahmanaldi290@gmail.com>
2018-07-06 11:27:32 +0700

    asd
    sad

---
+++ application/views/tes.php
@@ -0,0 +1,9 @@
+<div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+        	<p><img src="http://jic.ac.id/v2/images/jic-logo.png" alt="" width="124" height="50" /><p class="pull-right">Dateasdasasddddddddddddd</p>&nbsp;Jl.Perunggu No 53-54<br />&nbsp;Cempak Putih<br />&nbsp;Jakarta Pusat 10640</p>
+            
+        </div>
+    </div>
+  </div>
