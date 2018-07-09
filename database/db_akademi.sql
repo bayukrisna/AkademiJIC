e5bb4b3f1d03437d73975f0b4235a13c60124740
bayukrisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2018-07-09 13:42:59 +0700

    update db

--- database/db_akademi.sql
+++ database/db_akademi.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2018 at 09:28 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Generation Time: Jul 09, 2018 at 08:42 AM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.1.18
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `db_akademi`
@@ -26,15 +28,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `dosen`
 --
 
-CREATE TABLE IF NOT EXISTS `dosen` (
-  `id_dosen` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `dosen` (
+  `id_dosen` int(11) NOT NULL,
   `nama_dosen` varchar(255) NOT NULL,
   `kode_dosen` varchar(255) NOT NULL,
   `no_hp` varchar(250) NOT NULL,
-  `keterangan` text NOT NULL,
-  PRIMARY KEY (`id_dosen`),
-  UNIQUE KEY `nama_dosen` (`nama_dosen`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `dosen`
@@ -50,17 +50,16 @@ INSERT INTO `dosen` (`id_dosen`, `nama_dosen`, `kode_dosen`, `no_hp`, `keteranga
 -- Table structure for table `matkul_akuntansi`
 --
 
-CREATE TABLE IF NOT EXISTS `matkul_akuntansi` (
-  `id_matkul_akuntansi` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `matkul_akuntansi` (
+  `id_matkul_akuntansi` int(11) NOT NULL,
   `subject_code` varchar(20) NOT NULL,
   `subject_name` text NOT NULL,
   `credits` int(11) NOT NULL,
   `semester` int(11) NOT NULL,
   `dosen` varchar(20) NOT NULL,
   `kode` varchar(20) NOT NULL,
-  `keterangan` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_matkul_akuntansi`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `keterangan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `matkul_akuntansi`
@@ -75,7 +74,7 @@ INSERT INTO `matkul_akuntansi` (`id_matkul_akuntansi`, `subject_code`, `subject_
 -- Table structure for table `tb_du`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_du` (
+CREATE TABLE `tb_du` (
   `no_du` varchar(6) NOT NULL,
   `nama_du` varchar(100) NOT NULL,
   `jk_daftar_du` varchar(12) NOT NULL,
@@ -88,25 +87,24 @@ CREATE TABLE IF NOT EXISTS `tb_du` (
   `agama_du` text NOT NULL,
   `nik_du` varchar(50) NOT NULL,
   `ibu_kandung_du` text NOT NULL,
-  `id_sekolah` varchar(5) NOT NULL,
-  `id_prodi` varchar(5) NOT NULL,
-  `id_konsentrasi` varchar(6) NOT NULL,
+  `id_sekolah2` varchar(5) NOT NULL,
+  `id_prodi2` varchar(5) NOT NULL,
+  `id_konsentrasi2` varchar(6) NOT NULL,
   `intake` varchar(6) NOT NULL,
   `waktu` text NOT NULL,
   `status` enum('aktif','non-aktif') NOT NULL,
-  PRIMARY KEY (`no_du`),
-  KEY `id_sekolah` (`id_sekolah`),
-  KEY `id_prodi` (`id_prodi`),
-  KEY `id_prodi_2` (`id_prodi`),
-  KEY `id_konsentrasi` (`id_konsentrasi`)
+  `jurusan` varchar(20) NOT NULL,
+  `tanggal_du` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_du`
 --
 
-INSERT INTO `tb_du` (`no_du`, `nama_du`, `jk_daftar_du`, `tpt_lahir_du`, `tgl_lahir_du`, `alamat_du`, `no_telp_du`, `no_telpm_du`, `email_du`, `agama_du`, `nik_du`, `ibu_kandung_du`, `id_sekolah`, `id_prodi`, `id_konsentrasi`, `intake`, `waktu`, `status`) VALUES
-('PE001', 'Aldi', 'Male', '', '2018-05-01', 'Malang', '0987654', '987654', 'aldi@gmail.com', 'islam', '98765', 'Bu Wati', 'SE001', 'PR002', '', '', 'Pagi', 'aktif');
+INSERT INTO `tb_du` (`no_du`, `nama_du`, `jk_daftar_du`, `tpt_lahir_du`, `tgl_lahir_du`, `alamat_du`, `no_telp_du`, `no_telpm_du`, `email_du`, `agama_du`, `nik_du`, `ibu_kandung_du`, `id_sekolah2`, `id_prodi2`, `id_konsentrasi2`, `intake`, `waktu`, `status`, `jurusan`, `tanggal_du`) VALUES
+('DU009', 'Bayu', 'laki-laki', 'Malang', '2018-07-13', 'PAnjen', '877856', '678', 'bayuchrisna3@gmai.com', 'kristen', '43636', 'gerg', 'SE002', 'PR002', 'KO003', 'Januar', 'sore', 'aktif', 'rpl', '2018-07-08'),
+('DU010', 'Paris Hilton', 'perempuan', 'erter', '2018-07-13', 'ewew', '43643', '466', 'bayuchrisna3@gmai.com', 'islam', '4535', 'gerg', 'SE001', 'PR002', 'KO003', 'Februa', 'sore', 'aktif', 'tjk', '2018-07-09'),
+('DU011', 'Jlo', 'laki-laki', 'erter', '2018-07-09', 'efef', '3463', '46436', 'lady@gaga.com', 'buddha', '44353', 'ELy', 'SE001', 'PR001', 'KO001', 'Januar', 'Sore', 'aktif', 'hello', '2018-07-09');
 
 -- --------------------------------------------------------
 
@@ -114,14 +112,14 @@ INSERT INTO `tb_du` (`no_du`, `nama_du`, `jk_daftar_du`, `tpt_lahir_du`, `tgl_la
 -- Table structure for table `tb_hasiltes`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_hasiltes` (
+CREATE TABLE `tb_hasiltes` (
   `id_hasiltes` varchar(6) NOT NULL,
   `nilai_mat` int(11) NOT NULL,
   `nilai_bing` int(11) NOT NULL,
   `nilai_psikotes` int(11) NOT NULL,
-  `no_pendaftaran` varchar(6) NOT NULL,
-  PRIMARY KEY (`id_hasiltes`),
-  KEY `no_pendaftaran` (`no_pendaftaran`)
+  `total_nilai` int(11) NOT NULL,
+  `grade_tes` varchar(1) NOT NULL,
+  `id_pendaftaran2` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -130,10 +128,9 @@ CREATE TABLE IF NOT EXISTS `tb_hasiltes` (
 -- Table structure for table `tb_intake`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_intake` (
+CREATE TABLE `tb_intake` (
   `id_intake` varchar(6) NOT NULL,
-  `intake` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_intake`)
+  `intake` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -142,12 +139,10 @@ CREATE TABLE IF NOT EXISTS `tb_intake` (
 -- Table structure for table `tb_konsentrasi`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_konsentrasi` (
+CREATE TABLE `tb_konsentrasi` (
   `id_konsentrasi` varchar(6) NOT NULL,
   `nama_konsentrasi` varchar(20) NOT NULL,
-  `id_prodi2` varchar(6) NOT NULL,
-  PRIMARY KEY (`id_konsentrasi`),
-  KEY `id_prodi` (`id_prodi2`)
+  `id_prodi2` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -159,9 +154,9 @@ INSERT INTO `tb_konsentrasi` (`id_konsentrasi`, `nama_konsentrasi`, `id_prodi2`)
 ('KO002', 'Finance Management', 'PR004'),
 ('KO003', 'Auditing', 'PR002'),
 ('KO004', 'Taxation', 'PR002'),
-('KO005', 'Kupukupu', 'PR001'),
-('KO006', 'Gantrung', 'PR003'),
-('KO007', 'yuyu', 'PR002');
+('KO005', 'Kupukupuss', 'PR003'),
+('KO006', 'Gantrungsss', 'PR003'),
+('KO007', 'yuyul', 'PR002');
 
 -- --------------------------------------------------------
 
@@ -169,26 +164,32 @@ INSERT INTO `tb_konsentrasi` (`id_konsentrasi`, `nama_konsentrasi`, `id_prodi2`)
 -- Table structure for table `tb_pendaftaran`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_pendaftaran` (
+CREATE TABLE `tb_pendaftaran` (
   `id_pendaftaran` varchar(6) NOT NULL,
   `tanggal_pendaftaran` date NOT NULL,
   `nama_pendaftar` varchar(50) NOT NULL,
-  `id_sekolah` varchar(6) NOT NULL,
+  `id_sekolah2` varchar(6) NOT NULL,
   `jurusan` varchar(20) NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `email` varchar(30) NOT NULL,
   `no_telp` varchar(15) NOT NULL,
-  PRIMARY KEY (`id_pendaftaran`)
+  `waktu` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tb_pendaftaran`
 --
 
-INSERT INTO `tb_pendaftaran` (`id_pendaftaran`, `tanggal_pendaftaran`, `nama_pendaftar`, `id_sekolah`, `jurusan`, `alamat`, `email`, `no_telp`) VALUES
-('PE001', '2018-07-06', 'Jessica Jung', 'SE002', '', 'Jakarta Barat', 'bayuchrisna3@gmail.com', '009875'),
-('PE002', '2018-07-06', 'Jessica Jung', 'SE002', '', 'Jakarta Barat', 'bayuchrisna3@gmail.com', '009875'),
-('PE003', '2018-07-06', 'taeyeon', 'SE001', '', 'malang', 'bayu_krisna_24rpl@student.smkt', '545435');
+INSERT INTO `tb_pendaftaran` (`id_pendaftaran`, `tanggal_pendaftaran`, `nama_pendaftar`, `id_sekolah2`, `jurusan`, `alamat`, `email`, `no_telp`, `waktu`) VALUES
+('PE001', '2018-07-06', 'Jessica Jung', 'SE002', 'mb', 'Jakarta Barat', 'bayuchrisna3@gmail.com', '009875', ''),
+('PE002', '2018-07-06', 'Jessica Jung', 'SE002', 'mb', 'Jakarta Barat', 'bayuchrisna3@gmail.com', '009875', ''),
+('PE003', '2018-07-06', 'taeyeon', 'SE001', 'iya', 'malang', 'bayu_krisna_24rpl@student.smkt', '545435', ''),
+('PE004', '2018-07-08', 'yaaa', 'SE001', '', 'as', 'as@asd.sads', '123', ''),
+('PE005', '2018-07-08', 'aldi', 'SE002', 'TKJ', 'as', 'as@asd.sads', '1', ''),
+('PE006', '2018-07-08', 'LAdy Gaga', 'SE001', 'TKJ', 'Los Angeles', 'lady@gaga.com', '8987675645', ''),
+('PE007', '2018-07-08', 'Yoona', 'SE001', 'IPS', 'MAlang', 'yoona@gmail.com', '9786786', 'pagi'),
+('PE008', '2018-07-09', 'Paris ', 'SE001', 'IPA', 'gyugwd', 'as@asd.sads', '865786578', 'pagi'),
+('PE009', '2018-07-09', 'uyewf', 'SE001', 'IPA', 'wre', 'as@asd.sads', '53', 'pagi');
 
 -- --------------------------------------------------------
 
@@ -196,11 +197,10 @@ INSERT INTO `tb_pendaftaran` (`id_pendaftaran`, `tanggal_pendaftaran`, `nama_pen
 -- Table structure for table `tb_prodi`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_prodi` (
+CREATE TABLE `tb_prodi` (
   `id_prodi` varchar(6) NOT NULL,
   `nama_prodi` varchar(20) NOT NULL,
-  `ketua_prodi` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_prodi`)
+  `ketua_prodi` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -209,9 +209,9 @@ CREATE TABLE IF NOT EXISTS `tb_prodi` (
 
 INSERT INTO `tb_prodi` (`id_prodi`, `nama_prodi`, `ketua_prodi`) VALUES
 ('PR001', 'Management', 'Dr. Lucinta Luna'),
-('PR002', 'Accounting', 'Dra. Tiffany'),
+('PR002', 'Accounting', 'Dra. Tiffany Hwang'),
 ('PR003', 'halu', 'haa'),
-('PR004', 'Tata Boga', 'Dr. Boga'),
+('PR004', 'Tata Bogas', 'Dr. Bogasss'),
 ('PR005', 'wes', 'uyt');
 
 -- --------------------------------------------------------
@@ -220,11 +220,10 @@ INSERT INTO `tb_prodi` (`id_prodi`, `nama_prodi`, `ketua_prodi`) VALUES
 -- Table structure for table `tb_sekolah`
 --
 
-CREATE TABLE IF NOT EXISTS `tb_sekolah` (
+CREATE TABLE `tb_sekolah` (
   `id_sekolah` varchar(6) NOT NULL,
   `nama_sekolah` varchar(30) NOT NULL,
-  `alamat_sekolah` text NOT NULL,
-  PRIMARY KEY (`id_sekolah`)
+  `alamat_sekolah` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -236,6 +235,87 @@ INSERT INTO `tb_sekolah` (`id_sekolah`, `nama_sekolah`, `alamat_sekolah`) VALUES
 ('SE002', 'SMAN 3 Jakarta', 'Jl. Supriyadi No 47 jakarta');
 
 --
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`id_dosen`),
+  ADD UNIQUE KEY `nama_dosen` (`nama_dosen`);
+
+--
+-- Indexes for table `matkul_akuntansi`
+--
+ALTER TABLE `matkul_akuntansi`
+  ADD PRIMARY KEY (`id_matkul_akuntansi`);
+
+--
+-- Indexes for table `tb_du`
+--
+ALTER TABLE `tb_du`
+  ADD PRIMARY KEY (`no_du`),
+  ADD KEY `id_sekolah` (`id_sekolah2`),
+  ADD KEY `id_prodi` (`id_prodi2`),
+  ADD KEY `id_prodi_2` (`id_prodi2`),
+  ADD KEY `id_konsentrasi` (`id_konsentrasi2`);
+
+--
+-- Indexes for table `tb_hasiltes`
+--
+ALTER TABLE `tb_hasiltes`
+  ADD PRIMARY KEY (`id_hasiltes`),
+  ADD KEY `no_pendaftaran` (`id_pendaftaran2`);
+
+--
+-- Indexes for table `tb_intake`
+--
+ALTER TABLE `tb_intake`
+  ADD PRIMARY KEY (`id_intake`);
+
+--
+-- Indexes for table `tb_konsentrasi`
+--
+ALTER TABLE `tb_konsentrasi`
+  ADD PRIMARY KEY (`id_konsentrasi`),
+  ADD KEY `id_prodi` (`id_prodi2`);
+
+--
+-- Indexes for table `tb_pendaftaran`
+--
+ALTER TABLE `tb_pendaftaran`
+  ADD PRIMARY KEY (`id_pendaftaran`);
+
+--
+-- Indexes for table `tb_prodi`
+--
+ALTER TABLE `tb_prodi`
+  ADD PRIMARY KEY (`id_prodi`);
+
+--
+-- Indexes for table `tb_sekolah`
+--
+ALTER TABLE `tb_sekolah`
+  ADD PRIMARY KEY (`id_sekolah`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dosen`
+--
+ALTER TABLE `dosen`
+  MODIFY `id_dosen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `matkul_akuntansi`
+--
+ALTER TABLE `matkul_akuntansi`
+  MODIFY `id_matkul_akuntansi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -244,6 +324,7 @@ INSERT INTO `tb_sekolah` (`id_sekolah`, `nama_sekolah`, `alamat_sekolah`) VALUES
 --
 ALTER TABLE `tb_konsentrasi`
   ADD CONSTRAINT `tb_konsentrasi_ibfk_1` FOREIGN KEY (`id_prodi2`) REFERENCES `tb_prodi` (`id_prodi`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
