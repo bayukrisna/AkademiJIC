5776f172bdad3fb6fd6b6d66c264e3de9af74ba3
bayukrisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2018-07-10 08:39:05 +0700

    fe

--- application/controllers/hasil_tes.php
+++ application/controllers/hasil_tes.php
@@ -18,4 +18,16 @@ class Hasil_tes extends CI_Controller {
 				$data['main_view'] = 'hasil_tes_view';
 				$this->load->view('template', $data);
 	}
+
+	public function save_hasil_tes()
+	{
+			if($this->pendaftaran_model->save_hasil_tes() == TRUE){
+				$hasil_tes = $this->input->post('nama_pendaftar');
+				$this->session->set_flashdata('message', '<div class="col-md-12 alert alert-success"> esHasil t '.$hasil_tes.' berhasil didaftarkan. </div>');
+            	redirect('pendaftaran');
+			} else{
+				$this->session->set_flashdata('message', '<div class="col-md-12 alert alert-danger"> Username/password sudah ada. </div>');
+            	redirect('pendaftaran');
+			} 
+	} 
 }
