e9ee7d100429508173cafd129bc122d36b1df8cb
aldirahmanws <rahmanaldi290@gmail.com>
2018-07-12 13:10:07 +0700

    as

--- application/controllers/finance.php
+++ application/controllers/finance.php
@@ -15,5 +15,12 @@ class Finance extends CI_Controller {
 		$data['mahasiswa'] = $this->finance_model->data_mahasiswa();
 		$this->load->view('template', $data);
 	}
+	public function konfirmasi($id_pendaftaran){
+				$data['data_biaya'] = $this->biaya_sekolah_model->data_biaya();
+				$data['main_view'] = 'Biaya_sekolah/edit_biaya_sekolah_view';
+				$id_biaya = $this->uri->segment(3);
+				$data['edit'] = $this->biaya_sekolah_model->get_biaya_by_id($id_biaya);
+				$this->load->view('template', $data);
+	}
 
 }
