002a24d2709c88e432c8358a94af94e2996691b6
aldirahmanws <rahmanaldi290@gmail.com>
2018-07-13 11:20:57 +0700

    k

---
+++ application/views/Mahasiswa/data_mahasiswa_view.php
@@ -0,0 +1,59 @@
+<section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <?php echo $this->session->flashdata('message');?>
+              <h3 class="box-title">Data Biaya</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama</th>
+                  <th>Asal Sekolah</th>
+                  <th>Nama Prodi</th>
+                  <th>Nama Konsentrasi</th>
+                  <th>Waktu</th>
+                  <th>Status</th>
+                  <th>Aksi</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php 
+                $no = 0;
+                $alert = "'Apakah anda yakin mengapus data ini ?'";
+                foreach ($mahasiswa as $data) {
+                  echo '
+                  
+                <tr>
+                  <td>'.++$no.'</td>
+                  <td>'.$data->nama_du.'
+                  </td>
+                   <td>'.$data->nama_sekolah.'</td>
+                  <td>'.$data->nama_prodi.'</td>
+                  <td>'.$data->nama_konsentrasi.'</td>
+                  <td>'.$data->waktu.'</td>
+                  <td>'.$data->status_du.'</td>
+                  <td>
+                      <p>ea</p>
+                  </td>
+                </tr>
+                ';
+                
+              }
+              ?>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
